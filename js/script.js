53a816400092fac345ad3bdc7c6da080a97efdd4
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2025-11-11 13:12:45 +0100

    Adding auto-generate list of articles/posts

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles',
+    optTitleListSelectorA = '.titles a',
+    optTitleListSelectorAActiveClass = '.titles a.active';
+
 const titleClickHandler = function (event) {
     const clickedElement = this;
     const targetPost = document.querySelector(clickedElement.getAttribute('href'))
-    const activeLinks = document.querySelectorAll('.titles a.active');
-    const activePosts = document.querySelectorAll('.post')
+    const activeLinks = document.querySelectorAll(optTitleListSelectorAActiveClass);
+    const activePosts = document.querySelectorAll(optArticleSelector)
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
@@ -16,8 +22,39 @@ const titleClickHandler = function (event) {
     clickedElement.classList.add('active')
 }
 
-const links = document.querySelectorAll('.titles a');
+function generateTitleLinks() {
 
-for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
+    const titleList = document.querySelector(optTitleListSelector)
+    const articles = document.querySelectorAll(optArticleSelector)
+
+    let html = ''
+
+    for (let article of articles) {
+        /* get the article id */
+        const articleId = article.getAttribute('id')
+
+        /* find the title element */
+        /* get the title from the title element */
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML
+
+        /* create HTML of the link */
+
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+        /* insert link into titleList */
+        html += linkHTML
+
+    }
+
+    titleList.innerHTML = html
+
+    const links = document.querySelectorAll(optTitleListSelectorA);
+
+    links[0].classList.add('active')
+
+    for (let link of links) {
+        link.addEventListener('click', titleClickHandler);
+    }
 }
+
+generateTitleLinks();
