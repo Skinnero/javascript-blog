5b784378e2b2ee8b4d18bcf25484a273558a482f
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2025-11-13 11:44:42 +0100

    Adding eslint to project, changing commands in task runner, fixing issues from eslint in js file

--- js/script.js
+++ js/script.js
@@ -1,60 +1,60 @@
 const optArticleSelector = '.post',
-    optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles',
-    optTitleListSelectorA = '.titles a',
-    optTitleListSelectorAActiveClass = '.titles a.active';
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles',
+  optTitleListSelectorA = '.titles a',
+  optTitleListSelectorAActiveClass = '.titles a.active';
 
-const titleClickHandler = function (event) {
-    const clickedElement = this;
-    const targetPost = document.querySelector(clickedElement.getAttribute('href'))
-    const activeLinks = document.querySelectorAll(optTitleListSelectorAActiveClass);
-    const activePosts = document.querySelectorAll(optArticleSelector)
+const titleClickHandler = function () {
+  const clickedElement = this;
+  const targetPost = document.querySelector(clickedElement.getAttribute('href'));
+  const activeLinks = document.querySelectorAll(optTitleListSelectorAActiveClass);
+  const activePosts = document.querySelectorAll(optArticleSelector);
 
-    for (let activeLink of activeLinks) {
-        activeLink.classList.remove('active');
-    }
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
 
-    for (let activePost of activePosts) {
-        activePost.classList.remove('active')
-    }
+  for (let activePost of activePosts) {
+    activePost.classList.remove('active');
+  }
 
-    targetPost.classList.add('active')
-    clickedElement.classList.add('active')
-}
+  targetPost.classList.add('active');
+  clickedElement.classList.add('active');
+};
 
 function generateTitleLinks() {
 
-    const titleList = document.querySelector(optTitleListSelector)
-    const articles = document.querySelectorAll(optArticleSelector)
+  const titleList = document.querySelector(optTitleListSelector);
+  const articles = document.querySelectorAll(optArticleSelector);
 
-    let html = ''
+  let html = '';
 
-    for (let article of articles) {
-        /* get the article id */
-        const articleId = article.getAttribute('id')
+  for (let article of articles) {
+    /* get the article id */
+    const articleId = article.getAttribute('id');
 
-        /* find the title element */
-        /* get the title from the title element */
-        const articleTitle = article.querySelector(optTitleSelector).innerHTML
+    /* find the title element */
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-        /* create HTML of the link */
+    /* create HTML of the link */
 
-        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
 
-        /* insert link into titleList */
-        html += linkHTML
+    /* insert link into titleList */
+    html += linkHTML;
 
-    }
+  }
 
-    titleList.innerHTML = html
+  titleList.innerHTML = html;
 
-    const links = document.querySelectorAll(optTitleListSelectorA);
+  const links = document.querySelectorAll(optTitleListSelectorA);
 
-    links[0].classList.add('active')
+  links[0].classList.add('active');
 
-    for (let link of links) {
-        link.addEventListener('click', titleClickHandler);
-    }
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
 }
 
 generateTitleLinks();
